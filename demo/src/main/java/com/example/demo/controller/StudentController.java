dc54890cb4cc3a4e2c26dab323335a70b45e48dc
Nisal Jayathilaka <nisal7075399@gmail.com>
2023-07-03 19:31:41 +0530

    modify the controller

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
 
+    public void nnn(){
+        System.out.println("asasasasasasa");
+    }
+
+    public void mmmm(){
+        System.out.println("cvccvcccvcvcvcvcv");
+    }
     public void abc(){
         System.out.println("asaasasas");
     }
